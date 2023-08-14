58442a240c6a75c06d1441daee10cd87099ba998
Andrés Parra <pipe.parrab@gmail.com>
2023-08-14 01:14:06 -0500

    Intentado corregir rutas para despliegue.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import {
   About,
   Contact,
@@ -13,7 +13,13 @@ import {
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/portafolio">
+      <Routes>
+        <Route index path="/" component={Hero} />
+        <Route path="/#about" component={About} />
+        <Route path="/#work" component={Works} />
+        <Route path="/#contact" component={Contact} />
+      </Routes>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
@@ -24,7 +30,7 @@ const App = () => {
         <Tech />
         <Works />
         <Feedbacks />
-        <div className="relative z-0 ">
+        <div className="relative z-0">
           <Contact />
           <StarsCanvas />
         </div>
