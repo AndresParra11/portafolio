61ca51536dc1af87aa1710257d94eb1acf3df924
Andrés Parra <pipe.parrab@gmail.com>
2023-08-14 11:34:30 -0500

    Listo el renderizado de algunas tecnologías y redirigirlo a la CV para que vean todas.

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,45 +5,45 @@ import { useEffect, useState } from "react";
 
 const Tech = () => {
   const initialBalls = 6; // Número inicial de bolas a renderizar
-  const [showAllBalls, setShowAllBalls] = useState(false);
-
-  // Manejar la interacción del usuario para cargar todas las bolas
-  const handleLoadAllBalls = () => {
-    setShowAllBalls(true);
-  };
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Puedes ajustar este tiempo de carga según tus necesidades
-    const loadAllTimeout = setTimeout(() => {
-      setShowAllBalls(true);
-    }, 4000); // Esperar 2 segundos antes de cargar todas las bolas
+    // Verificar si la vista actual es móvil
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    setIsMobile(mediaQuery.matches);
 
+    // Establecer el observador para detectar cambios en la vista móvil
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Limpiar el observador cuando el componente se desmonte
     return () => {
-      // Limpiar el timeout si el componente se desmonta antes de cargar todas las bolas
-      clearTimeout(loadAllTimeout);
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
   }, []);
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology, index) => (
         <div className="w-28 h-28" key={technology.name}>
-          {/* Renderizar todas las bolas si showAllBalls es verdadero */}
-          {showAllBalls || index < initialBalls ? (
+          {/* Renderizar todas las tecnologías si no es móvil o mostrar las primeras 6 si es móvil */}
+          {!isMobile || index < initialBalls ? (
             <BallCanvas icon={technology.icon} />
           ) : null}
         </div>
       ))}
-      {/*  {!showAllBalls && (
-        // Mostrar un botón para cargar todas las bolas cuando no se han cargado todas
+      {isMobile && (
+        // Mostrar un mensaje con un enlace para ver todas las tecnologías solo en la vista móvil
         <div className="flex justify-center mt-4">
-          <button
-            onClick={handleLoadAllBalls}
+          <a
+            href="https://drive.google.com/file/d/1tsXm5udEqkxos-MDrhWjBwKAXEA9V6hY/view?usp=drive_link"
             className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
           >
-            Cargar todas las Tecnologías
-          </button>
+            Ver todas las Tecnologías en la hoja de vida!
+          </a>
         </div>
-      )} */}
+      )}
     </div>
   );
 };
