d65f6e94a0bcb1ab33f09004ca64bc5080bea2a4
Andrés Parra <pipe.parrab@gmail.com>
2023-08-14 11:07:35 -0500

    Intentado renderizar un número menor de tecnologías y luego todas. Para mejorar en mobile y evitar errores.

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,15 +1,37 @@
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { useState } from "react";
 
 const Tech = () => {
+  const initialBalls = 5; // Número inicial de bolas a renderizar
+  const [showAllBalls, setShowAllBalls] = useState(false);
+
+  // Manejar la interacción del usuario para cargar todas las bolas
+  const handleLoadAllBalls = () => {
+    setShowAllBalls(true);
+  };
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
+      {technologies.map((technology, index) => (
         <div className="w-28 h-28" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+          {/* Renderizar todas las bolas si showAllBalls es verdadero */}
+          {showAllBalls || index < initialBalls ? (
+            <BallCanvas icon={technology.icon} />
+          ) : null}
         </div>
       ))}
+      {!showAllBalls && (
+        // Mostrar un botón para cargar todas las bolas cuando no se han cargado todas
+        <div className="flex justify-center mt-4">
+          <button
+            onClick={handleLoadAllBalls}
+            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary"
+          >
+            Cargar todas las Tecnologías
+          </button>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -15,25 +15,20 @@ const Ball = (props) => {
 
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      {/* <ambientLight intensity={0.25} /> */}
-      <ambientLight intensity={0.2} />
-
-      {/* <directionalLight position={[0, 0, 0.05]} /> */}
+      <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
-        {/* <icosahedronGeometry args={[1, 1]} /> */}
-        <sphereGeometry args={[1, 8, 8]} />
+        <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
           color="#fff8eb"
-          /* polygonOffset
-          polygonOffsetFactor={-5} */
+          polygonOffset
+          polygonOffsetFactor={-5}
           flatShading
         />
         <Decal
           position={[0, 0, 1]}
           rotation={[2 * Math.PI, 0, 6.25]}
-          /* scale={1} */
-          scale={0.75}
+          scale={1}
           map={decal}
           flatShading
         />
